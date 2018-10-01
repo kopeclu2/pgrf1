98eaa702f517e6b7a3847daf1dfef6dd44d08218
a <a@a.a>
2018-10-01 12:10:21 +0200

    cara

--- src/utils/Renderer.java
+++ src/utils/Renderer.java
@@ -16,39 +16,52 @@ public class Renderer {
         img.setRGB(x,y,color);
     }
 
-    public void lineTrivial(int x1, int y1,int x2, int y2){
+    public void lineTrivial(int x1, int y1,int x2, int y2) {
         //y=kx+q
-        int dx = x1-x2; //rozdil prvni x a vystupni x
-        int dy = y1-y2; //-||-
+        int dx = x1 - x2; //rozdil prvni x a vystupni x
+        int dy = y1 - y2; //-||-
 
-        if (Math.abs(dx)> Math.abs(dy)){
+        if (Math.abs(dx) > Math.abs(dy)) {
             //ridici osa X
             //otoceni souradnic
 
-            if (x1>x2){//prohozeni promenych
+            if (x1 > x2) {//prohozeni promenych
                 int p = x1;
-                x1=x2;
-                x2=p;
-                p=y1;
-                y1=y2;
+                x1 = x2;
+                x2 = p;
+                p = y1;
+                y1 = y2;
             }
-            float k = (float)dy / (float)dx;//zavislot x na yx o kolik se zvedne
+            float k = (float) dy / (float) dx;//zavislot x na yx o kolik se zvedne
             for (int x = x1; x < x2; x++) {
-                int y = y1 + (int)(k*(x-x1));
-                drawPixel(x,y);//q = pocatek y souradnice
+                int y = y1 + (int) (k * (x - x1));
+                drawPixel(x, y);//q = pocatek y souradnice
             }
 
 
-        }else {
+        } else {
+            if (Math.abs(dy) > Math.abs(dx)) {
+                //ridici osa X
+                //otoceni souradnic
+
+                if (y1 > y2) {//prohozeni promenych
+                    int p = y1;
+                    y1 = y2;
+                    y2 = p;
+                    p = x1;
+                    x1 = x2;
+                }
+                float k = (float) dx / (float) dy;//zavislot x na yx o kolik se zvedne
+                for (int y = y1; y < y2; y++) {
+                    int x = x1 + (int) (k * (y - y1));
+                    drawPixel(x, y);//q = pocatek y souradnice
+                }
+
+
+            }
 
 
         }
 
 
-
-    }
-
-
-
-
-}
+    }}
